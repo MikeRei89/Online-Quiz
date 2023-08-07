a493bad2137bd9caf658581c6b50d9f511b8cedb
Mike Rei <mikereilly5@gmail.com>
2023-08-07 15:28:08 -0400

    updated jscript to add const and fuinctions

--- script.js
+++ script.js
@@ -1,35 +1,22 @@
 const startScreen = document.querySelector(".startScreen");
-const win = document.querySelector(".win");
-const lose = document.querySelector(".lose");
+const scoreContainer = document.querySelector(".scoreContainer");
 const timerElement = document.querySelector(".timer-count");
 const startButton = document.querySelector(".start-button");
 const choicesList = document.querySelector("#choices")
 
 let Score = 0;
-let isWin = false;
+// let isWin = false;
 let timer;
 let timerCount;
 
-// Arrays used to create blanks and letters on screen
-var lettersInChosenWord = [];
-var blanksLetters = [];
-
-// Array of words the user will guess
-// var words = ["variable","array", "modulus", "object", "function", "string", "boolean"];
-
-// The init function is called when the page loads 
 function init() {
-  getWins();
-  getlosses();
+  Score();
 }
 
-// The startGame function is called when the start button is clicked
 function startGame() {
   isWin = false;
-  timerCount = 10;
-  // Prevents start button from being clicked when round is in progress
+  timerCount = 30;
   startButton.disabled = true;
-  renderBlanks()
   startTimer()
 }
 
@@ -72,31 +59,18 @@ function startTimer() {
   }, 1000);
 }
 
-// Creates blanks on screen
+
 function renderBlanks() {
-  // Randomly picks word from words array
-  chosenWord = words[Math.floor(Math.random() * words.length)];
-  lettersInChosenWord = chosenWord.split("");
-  numBlanks = lettersInChosenWord.length;
-  blanksLetters = []
   // Uses loop to push blanks to blankLetters array
   for (var i = 0; i < numBlanks; i++) {
     blanksLetters.push("_");
   }
-  // Converts blankLetters array into a string and renders it on the screen
-  wordBlank.textContent = blanksLetters.join(" ")
 }
 
 // Updates win count on screen and sets win count to client storage
-function setWins() {
-  win.textContent = winCounter;
-  localStorage.setItem("winCount", winCounter);
-}
-
-// Updates lose count on screen and sets lose count to client storage
-function setLosses() {
-  lose.textContent = loseCounter;
-  localStorage.setItem("loseCount", loseCounter);
+function Score() {
+  score.textContent = Score;
+  localStorage.setItem("Score", score);
 }
 
 // These functions are used by init
@@ -114,39 +88,11 @@ function getWins() {
   win.textContent = winCounter;
 }
 
-function getlosses() {
-  var storedLosses = localStorage.getItem("loseCount");
-  if (storedLosses === null) {
-    loseCounter = 0;
-  } else {
-    loseCounter = storedLosses;
-  }
-  lose.textContent = loseCounter;
-}
-
 function checkWin() {
   // If the word equals the blankLetters array when converted to string, set isWin to true
   if (chosenWord === blanksLetters.join("")) {
     // This value is used in the timer function to test if win condition is met
     isWin = true;
-  }
-}
-
-// Tests if guessed letter is in word and renders it to the screen.
-function checkLetters(letter) {
-  var letterInWord = false;
-  for (var i = 0; i < numBlanks; i++) {
-    if (chosenWord[i] === letter) {
-      letterInWord = true;
-    }
-  }
-  if (letterInWord) {
-    for (var j = 0; j < numBlanks; j++) {
-      if (chosenWord[j] === letter) {
-        blanksLetters[j] = letter;
-      }
-    }
-    wordBlank.textContent = blanksLetters.join(" ");
   }
 }
 
@@ -167,7 +113,6 @@ document.addEventListener("keydown", function(event) {
   }
 });
 
-// Attach event listener to start button to call startGame function on click
 startButton.addEventListener("click", startGame);
 
 // Calls init() so that it fires when page opened
@@ -177,12 +122,10 @@ init();
 var resetButton = document.querySelector(".reset-button");
 
 function resetGame() {
-  // Resets win and loss counts
-  winCounter = 0;
-  loseCounter = 0;
-  // Renders win and loss counts and sets them into client storage
-  setWins()
-  setLosses()
+  // Resets score counts
+  Score = 0;
+  // Renders score counts and sets them into client storage
+  setScore()
 }
 // Attaches event listener to button
 resetButton.addEventListener("click", resetGame);
